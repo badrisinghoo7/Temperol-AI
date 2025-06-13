cf2d2f8a85634b55bf110fd5b05c1c3f3e057b79
badrisinghoo7 <you@example.com>
2025-06-13 22:41:14 +0530

    adding some changes

--- backend/activities/userActivities.ts
+++ backend/activities/userActivities.ts
@@ -2,14 +2,15 @@ import axios from 'axios';
 import User from '../models/User';
 
 export async function saveToDB(data: any) {
-  await User.findOneAndUpdate(
+  let result = await User.findOneAndUpdate(
   { authId: data.authId },
   data,
   { upsert: true, new: true }
 );
+return result;
 }
 
-export async function saveToCrudCrud(data: any) {
-  const api = process.env.CRUD_CRUD_API!;
-  await axios.post(`${api}/profile`, data);
-}
+// export async function saveToCrudCrud(data: any) {
+//   const api = process.env.CRUD_CRUD_API!;
+//   await axios.post(`${api}/profile`, data);
+// }
--- backend/workflows/userWorkflow.ts
+++ backend/workflows/userWorkflow.ts
@@ -1,10 +1,8 @@
 import { proxyActivities } from '@temporalio/workflow';
-const { saveToDB, saveToCrudCrud } = proxyActivities({
+const { saveToDB } = proxyActivities({
   startToCloseTimeout: '30 seconds',
 });
 
 export async function updateUserWorkflow(userData: any) {
   await saveToDB(userData);
-  await new Promise((r) => setTimeout(r, 10000));
-  await saveToCrudCrud(userData);
 }
